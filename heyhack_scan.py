7f07f268f5b038dcf806a654dc6d733a36103e8d
Mao301 <mauricioocampo301@gmail.com>
2024-08-15 10:09:22 -0500

    Add Azure DevOps pipeline and Heyhack scan script new

--- heyhack_scan.py
+++ heyhack_scan.py
@@ -3,22 +3,30 @@ import json
 import os
 
 def main():
-    heyhack_api_key = os.environ['HEYHACK_API_KEY']
-    target_url = os.environ['TARGET_URL']
-    heyhack_url = "https://app.heyhack.com/img/scanning.json"
+    heyhack_api_key = os.environ['23UdMkr/F+6FirDbAHFjsPhenpMg3Y2EviuEKxCLjrE=']
+    target_url = os.environ['https://arcadia.f5latam.app/']
+    #heyhack_url = "https://app.heyhack.com/img/scanning.json"
+    heyhack_url = "https://app.heyhack.com/api/scanjobs"
+    profile_id = "3b2f8285-6cb8-40d4-a286-b4d07870f36e"
+    application_id = "9c098294-da9f-4529-b424-9a8a88efc7b5"
     
     # Configura el payload para iniciar el escaneo
     payload = {
-        "target": target_url,  # URL objetivo del escaneo
-        "scan_type": "full"
+        #"target": target_url,  # URL objetivo del escaneo
+        #"scan_type": "full"
+        
+        "profile_id": profile_id,
+        "application_id": application_id
     }
     
     headers = {
-        "Authorization": f"Bearer {heyhack_api_key}",
-        "Content-Type": "application/json"
+        #"Authorization": f"Bearer {heyhack_api_key}",
+        "Authorization": 'Heyhack 23UdMkr/F+6FirDbAHFjsPhenpMg3Y2EviuEKxCLjrE=' ,
+        #"Content-Type": "application/json"
     }
     
-    response = requests.post(heyhack_url, headers=headers, data=json.dumps(payload))
+    #response = requests.post(heyhack_url, headers=headers, data=json.dumps(payload))
+    response = requests.post(heyhack_url, headers=headers, data=payload)
     
     if response.status_code == 200:
         print("Scan initiated successfully")

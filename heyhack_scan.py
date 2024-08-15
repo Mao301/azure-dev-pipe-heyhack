700bac9711012df785a3e07a6741a64f4888721a
Mao301 <mauricioocampo301@gmail.com>
2024-08-15 11:08:30 -0500

    Add Azure DevOps pipeline and Heyhack scan script new

--- heyhack_scan.py
+++ heyhack_scan.py
@@ -4,17 +4,14 @@ import os
 
 def main():
     #heyhack_api_key = os.environ['23UdMkr/F+6FirDbAHFjsPhenpMg3Y2EviuEKxCLjrE=']
+    heyhack_api_key = os.environ['HEYHACK_API_KEY']
     #target_url = os.environ['https://arcadia.f5latam.app/']
-    #heyhack_url = "https://app.heyhack.com/img/scanning.json"
     heyhack_url = "https://app.heyhack.com/api/scanjobs"
     profile_id = "3b2f8285-6cb8-40d4-a286-b4d07870f36e"
     application_id = "9c098294-da9f-4529-b424-9a8a88efc7b5"
     
     # Configura el payload para iniciar el escaneo
     params = {
-        #"target": target_url,  # URL objetivo del escaneo
-        #"scan_type": "full"
-        
         "profile_id": profile_id,
         "application_id": application_id
     }
@@ -22,7 +19,8 @@ def main():
     headers = {
         #"Authorization": f"Bearer {heyhack_api_key}",
         "accept": "*/*",
-        "Authorization": 'Heyhack 23UdMkr/F+6FirDbAHFjsPhenpMg3Y2EviuEKxCLjrE='
+        #"Authorization": 'Heyhack 23UdMkr/F+6FirDbAHFjsPhenpMg3Y2EviuEKxCLjrE='
+        "Authorization": 'Heyhack ' + heyhack_api_key
         #"Content-Type": "application/json"
         
     }
